29e42905937a7ad50debb4aa68f10ad75b36c09f
AngelDKarlo <2430257@upv.edu.mx>
2025-09-23 08:53:27 -0600

    agrege cantidad total medallas pais y tletas

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream> //libreria para conversion y manpulacion de texto
 #include <fstream> //libreria para lectura y escritura de archivos
+#include <limits>
 
 
 void mostrarInformacionAtleta(std::string nombre[], std::string pais[], std::string diciplina[], std::string genero[], int medallas[], std::string nombreAtleta, int n){
@@ -22,11 +23,57 @@ void mostrarInformacionAtleta(std::string nombre[], std::string pais[], std::str
     std::cout << "No se encontro al Atleta" << std::endl;
 }
 
-void medallasTotalesPais(std::string pais[], int medallas[], int n, std::string paisElegido){
+void medallasTotalesPais(std::string pais[], int medallas[], int n){
+   std::string paisesUnicos[n] ={};
+   int contador = 0;
 
 
+   for( int i = 0; i <n; i++){
+        bool paisU = true;
+
+        for(int j = 0; j <n;j++){
+            if(pais[i] == paisesUnicos[j]){
+                paisU  = false;
+                break;
+            }
+        }
+
+        if(paisU){
+            paisesUnicos[contador] = pais[i];
+            contador++;
+        }         
+    }
+
+
+    for(int i = 0; i <contador; i++){
+        std::string paisActual = paisesUnicos[i];
+        int medallasTotales = 0;
+        for(int j = 0; j < n; j++){
+            if(paisActual == pais[j]){
+                medallasTotales = medallasTotales + medallas[j];
+            }
+        }
+        std::cout << "Las medallas de " << paisActual << " son " << medallasTotales << std::endl;
+    }
 }
 
+
+void mayorCantMedallas(int medallas[], std::string atleta[], int n){
+    int mayorCant = medallas[0];
+    std::string atletaMayor = atleta[0];
+    for(int i = 0; i <n; i++){
+        if(medallas[i] > mayorCant){
+            mayorCant = medallas[i];
+            atletaMayor = atleta[i];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        if(mayorCant == medallas[i]){
+            std::cout << "El atleta " << atleta[i] << " tiene " << medallas[i] << " medallas"<< std::endl;
+        }        
+    }
+}
 
 int main(int s, char *args[]) {
     std::string linea; //para leer cada una de las lineas del archivo
@@ -39,9 +86,13 @@ int main(int s, char *args[]) {
     std::string nombreArchivo;
     
     
-    nombreArchivo=args[1];
-    //std::cout << "Cual es el nombre del archivo csv?" << std::endl;
-    //std::cin >>nombreArchivo;
+    if(s < 2) {
+        std::cout << "No se ingreso un archivo" << std::endl;
+        std::cin >> nombreArchivo;
+            
+    }else{
+        nombreArchivo=args[1];
+    }
     
     std::ifstream archivoParaTamano(nombreArchivo);
     std::getline(archivoParaTamano, linea);
@@ -80,14 +131,30 @@ int main(int s, char *args[]) {
         std::cout << "-------------------------------------" << std::endl;
         std::cout << "Que desea hacer?" << std::endl;
         std::cout << "1- Mostrar informacion de un atleta" << std::endl;
+        std::cout << "2- Medallas de oro totales de un pais" << std::endl;
+        std::cout << "3- Atleta con la mayor cantidad de medallas" << std::endl;
         std::cout << "0- Salir" << std::endl;
         std::cout << "-------------------------------------" << std::endl;
         std::cin >> opcion;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         switch(opcion){
             case 1: 
                 std::cout << "Cual es el nombre del atleta?" << std::endl;
-                std::cin >> nom;
+
+                std::getline(std::cin, nom);
                 mostrarInformacionAtleta(nombre, pais, diciplina, genero, medallas,nom,  n);
+                break;
+            case 2:
+                medallasTotalesPais(pais, medallas, n);
+                break;
+            case 3:
+                mayorCantMedallas( medallas, nombre, n);
+                break;
+            case 0: 
+                break;
+            default:
+                std::cout << "Seleccione una opcion valida" << std::endl;
+                break;
         }
     
 
